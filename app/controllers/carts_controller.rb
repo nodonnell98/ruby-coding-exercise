b7e24d6f44721544c3edd1c9aba2caa3c1234d66
Niall O'Donnell <niall_od98@hotmail.com>
2023-10-25 14:08:50 +0100

    add cart show page and cart link in product show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,29 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :add_to_cart]
   def show
-    @cart = current_cart
+  end
+
+  def add_to_cart
+    product = Product.find(params[:product_id])
+    @cart_item = @cart.cart_items.find_by(product: product)
+  
+    if @cart_item
+      @cart_item.quantity += 1
+    else
+      @cart_item = @cart.cart_items.build(product: product, quantity: 1)
+    end
+  
+    if @cart_item.save
+      redirect_to cart_path, notice: 'Product added to cart!'
+    else
+      redirect_to products_path, alert: 'Could not add product to cart.'
+    end
   end
 
   private
 
-  def current_cart
-    Cart.first_or_create
+  def set_cart
+    @cart = Cart.first_or_create
   end
   
 end
